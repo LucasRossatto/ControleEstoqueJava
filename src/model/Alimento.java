8724279ea7ee1b241fec61bc163f8f7d1883a6fb
LucasRossatto <lucas.sant.anagn3@gmail.com>
2024-04-03 16:43:09 -0300

    main

---
+++ src/model/Alimento.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Alimento extends Produto {
+
+	private String strDataValidade;
+
+	public String getStrDataValidade() {
+		return strDataValidade;
+	}
+
+	public void setStrDataValidade(String strDataValidade) {
+		this.strDataValidade = strDataValidade;
+	}
+}
---
+++ src/model/Eletronico.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Eletronico extends Produto{
+
+	private String strMarca;
+	private String strModelo;
+	
+	public String getStrMarca() {
+		return strMarca;
+	}
+	public void setStrMarca(String strMarca) {
+		this.strMarca = strMarca;
+	}
+	public String getStrModelo() {
+		return strModelo;
+	}
+	public void setStrModelo(String strModelo) {
+		this.strModelo = strModelo;
+	}
+
+}
